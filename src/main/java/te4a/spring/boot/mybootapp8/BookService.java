94ab055e960cc39f5f91ec6be7671159f35baff1
石川 雅述 <190187@jc-21.jp>
2022-05-24 11:50:02 +0900

    Add devtools to hotreload

--- src/main/java/te4a/spring/boot/mybootapp8/BookService.java
+++ src/main/java/te4a/spring/boot/mybootapp8/BookService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ch.qos.logback.core.joran.util.beans.BeanUtil;
-
 @Service
 public class BookService {
     @Autowired
