8127fc7cdc22642689b19bde8cde1b41ab0f2bb5
石川 雅述 <190187@jc-21.jp>
2022-05-24 10:47:50 +0900

    Refactor

--- src/main/java/te4a/spring/boot/mybootapp8/BookController.java
+++ src/main/java/te4a/spring/boot/mybootapp8/BookController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HelloController {
+public class BookController {
     @Autowired
     BookService bookService;
 
---
+++ src/main/java/te4a/spring/boot/mybootapp8/BookForm.java
@@ -0,0 +1,5 @@
+package te4a.spring.boot.mybootapp8;
+
+public class BookForm {
+    
+}
