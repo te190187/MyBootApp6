66858a70bb776e32b2c210f327114507877b2240
石川 雅述 <190187@jc-21.jp>
2022-05-24 10:35:00 +0900

    Rename

--- src/test/java/te4a/spring/boot/mybootapp7/Mybootapp7ApplicationTests.java
+++ src/test/java/te4a/spring/boot/mybootapp7/Mybootapp7ApplicationTests.java
@@ -1,10 +1,10 @@
-package te4a.spring.boot.mybootapp6;
+package te4a.spring.boot.mybootapp7;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Mybootapp6ApplicationTests {
+class Mybootapp7ApplicationTests {
 
 	@Test
 	void contextLoads() {
